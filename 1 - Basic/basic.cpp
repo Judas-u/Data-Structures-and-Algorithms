beed483317c03ed83793797f760cea5cdd9b88e7
Judas-u <adishwarraj9905@gmail.com>
2025-01-12 17:22:43 +0530

    basic question

--- 1 - Basic/basic.cpp
+++ 1 - Basic/basic.cpp
@@ -1,1 +1,27 @@
-// for testing
+#include<iostream>
+using namespace std;
+    int removeElement(int nums[], int n, int val){
+        int k = 0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] != val){
+                nums[k++] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+
+    int main() {
+        int n, val;
+        cin >> n;
+
+        int nums[n];
+        for(int i=0; i<n; i++){
+            cin >> nums[i];
+        }
+        cin >> val;
+    
+    return 0;
+
+}
