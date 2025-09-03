afe4929fed30ef5fb45b726464f23b7e1c542803
Judas-u <adishwarraj9905@gmail.com>
2025-09-03 22:56:09 +0530

    Array-Basics

---
+++ 7 - Array/Array - Basics/_5_Linear_Search.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+int main () {
+
+    int n;
+    cout << "Enter size of Array : ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter Arrays : ";
+    for(int i=0; i<=n-1; i++){
+        cin >> arr[i];
+    }
+
+    int x;
+    cout << "Enter the element you want to search : ";
+    cin >> x;
+
+    // Search _____________________________
+    // Check Marks ________________________
+
+    bool flag = false;
+    for(int i=0; i<=n-1; i++){
+        if(arr[i] == x) flag = true; 
+    }
+    if(flag == true) cout << "Element found ! Congrats";
+    else cout << "Not Present" << " " << endl;
+    
+}
