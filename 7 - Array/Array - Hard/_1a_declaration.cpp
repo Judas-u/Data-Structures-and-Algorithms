aa456072d11f789895245b4f466d5bcd1000d8cc
Judas-u <adishwarraj9905@gmail.com>
2025-09-14 22:25:39 +0530

    Array-Hard

---
+++ 7 - Array/Array - Hard/_1a_declaration.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[3][3] = {1,4,5,6,2,5,9,4,8};   // both are same 
+
+    int arr_2[3][3] = {{1,4,5},{6,2,5},{9,4,8}}; // both are same 
+
+    cout <<arr << endl;
+    cout <<arr_2 << endl;
+}
