9fb3d831bcca0fe3690bf8f39e3f96ac81dda338
Judas-u <adishwarraj9905@gmail.com>
2025-09-15 14:26:02 +0530

    Array-Hard

---
+++ 7 - Array/Array - Hard/_3c_sum-all-matrix.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main() {
+    int m, n;
+    cout << "Enter the number of rows: ";
+    cin >> m;
+    cout << "Enter the number of columns: ";
+    cin >> n;
+
+    int arr[m][n];
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i <= m-1; i++) {
+        for (int j = 0; j <= n-1; j++) { 
+            cin >> arr[i][j];
+        }
+    }
+
+    int sum = 0;
+    for (int i = 0; i <= m-1; i++) {
+        for (int j = 0; j <= n-1; j++) {
+            sum += arr[i][j];
+        }
+    }
+    cout << "Sum element: " << sum<< endl;
+
+}
