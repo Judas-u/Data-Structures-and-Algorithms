b10ec7df371a92dbb70365df3d2e90308f2afd2a
Judas-u <adishwarraj9905@gmail.com>
2025-09-24 23:50:41 +0530

    Strings

---
+++ 8 - Strings/_6b_subString.cpp
@@ -0,0 +1,17 @@
+
+//____ ____  " substr() "  Operator  ____ ____ ____ ____ ____
+
+#include<iostream>
+#include<string>
+#include<algorithm> 
+
+using namespace std;
+
+int main () {
+            //  012344567
+    string s = "abcdefghi";
+
+    // s.substr(idx, len)
+    cout << s.substr(2, 5);
+
+}
