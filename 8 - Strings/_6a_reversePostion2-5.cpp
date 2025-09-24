9d7be46285f7d31d171515f17595664e36c1991c
Judas-u <adishwarraj9905@gmail.com>
2025-09-24 23:50:34 +0530

    Strings

---
+++ 8 - Strings/_6a_reversePostion2-5.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+#include<string>
+#include<algorithm> 
+
+using namespace std;
+
+int main () {
+    string s ;
+    getline(cin, s);
+
+    reverse(s.begin()+1, s.begin()+5);
+    cout << s;
+}
