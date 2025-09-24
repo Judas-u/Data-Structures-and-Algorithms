0ca8dc6c03db483a1ad7833a8c7b2e706e3418d1
Judas-u <adishwarraj9905@gmail.com>
2025-09-24 23:50:48 +0530

    Strings

---
+++ 8 - Strings/_6c_subString.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main () {
+    string s;
+    cout << "Enter the string : ";
+    cin >> s;
+
+    int len = s.length();
+    cout << s.substr(len/2);
+}
